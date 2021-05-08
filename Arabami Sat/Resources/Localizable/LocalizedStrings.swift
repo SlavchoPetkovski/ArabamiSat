d6fbfdb5895788a78be9ac163fd7d1a2e9dcdd6b
Slavcho Petkovski <slavcho.petkovski@seavus.com>
2021-05-09 00:35:24 +0200

    Added localization.

---
+++ Arabami Sat/Resources/Localizable/LocalizedStrings.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStrings.swift
+//  Arabami Sat
+//
+//  Created by Slavcho Petkovski on 8.5.21.
+//
+
+import Foundation
+
+struct Strings {
+    static let LoginButtonTitle = NSLocalizedString("FB_Login_Button_Title", comment: "")
+    static let GoogleButtonTitle = NSLocalizedString("Google_Login_Button_Title", comment: "")
+    static let OKButton = NSLocalizedString("OK", comment: "")
+    static let Error = NSLocalizedString("Error", comment: "")
+    static let LoginError = NSLocalizedString("LoginError", comment: "")
+    static let UnknownUser = NSLocalizedString("UnknownUser", comment: "")
+}
